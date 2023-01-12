ca53431675555fd0a187a9dbe45a89668224c043
Hyunseon <sunny20829@gmail.com>
2023-01-12 15:56:52 +0900

    INIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import style from './style.module.css';
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo('');
+  };
+  const deleteToDo = (index) =>
+    setToDos((currentArray) =>
+      currentArray.filter((_, currentIndex) => currentIndex !== index)
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Please write your main task"
+        />
+      </form>
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button className={style.btn} onClick={() => deleteToDo(index)}>
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
